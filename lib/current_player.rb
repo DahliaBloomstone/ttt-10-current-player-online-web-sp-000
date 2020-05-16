0986a68feef56c55c9cb759e997aff1ea084c81e
Dahlia Bloomstone <dahliabloomstone@gmail.com>
2020-05-16 21:03:27 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,8 +1,15 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-def turn_count
-counter = 0
-board.each do |turn|
-  puts "X"
-  puts "O"
-  counter += 1
+def turn_count(board)
+turns = 0
+board.each do |space|
+  if space == "X" || space == "O"
+  turns += 1
+end
+end
+return turns
+end
+
+def current_player(board)
+
+
 end
